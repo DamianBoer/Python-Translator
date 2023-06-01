6d5ed5e3e67911116e6621dd41d4e81b580d4fa3
Damian Boersma <114180492+DamianBoer@users.noreply.github.com>
2023-05-31 20:02:15 -0400

    Added Gitignore

---
+++ Brain.py
@@ -0,0 +1,14 @@
+from translate import Translator
+# Installed using pip install translate
+
+trans_lang = ['fr', 'ja', 'ar']
+
+def Translate():
+
+    text = input("What text would you like to translate: ")
+
+    with open('translated.txt', 'w', encoding='utf-8') as file:
+        for i in trans_lang:
+            translator = Translator(from_lang='en', to_lang=i)
+            translation = translator.translate(text)
+            file.write(f"{i}: {translation}\n") 
--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-from translate import Translator
-# Installed using pip install translate
+import Brain as br
 
-trans_lang = ['fr', 'ja', 'ar']
-
-def TranslatePhrase():
-
-    text = input("What text would you like to translate: ")
-
-    with open('translate.txt', 'w', encoding='utf-8') as file:
-        for i in trans_lang:
-            translator = Translator(from_lang='en', to_lang=i)
-            translation = translator.translate(text)
-            file.write(f"{i}: {translation}\n")
-
-TranslatePhrase()
+br.Translate()
